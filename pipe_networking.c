6037cf72a185ee8e68a5b52a8f6421ea87104211
Ricky Chen <rchen10@stuy.edu>
2017-12-12 11:46:43 -0500

    start

--- pipe_networking.c
+++ pipe_networking.c
@@ -11,6 +11,15 @@
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_handshake(int *to_client) {
+  if (mkfifo(ACK, 0600))
+    printf("Server error: %s\n", strerror(errno));
+
+  int from_client = open(ACK, O_RDONLY, 0600);
+
+  
+  char *fifo_name = (char *)calloc(BUFFER_SIZE, sizeof(char));
+  read(from_client, fifo_name, BUFFER_SIZE);
+  *to_client = open(fifo_name, O0600);
   return 0;
 }
 
@@ -25,5 +34,15 @@ int server_handshake(int *to_client) {
   returns the file descriptor for the downstream pipe.
   =========================*/
 int client_handshake(int *to_server) {
+  if (mkfifo("CLIENT", 0600))
+    printf("Client error: %s\n", strerror(errno));
+
+  int from_server = open("CLIENT", 
+  *to_server = open(ACK, O_WRONLY, 0600);
+
+  //sending private fifo
+  char *fifo_name = (char *)calloc(BUFFER_SIZE, sizeof(char));
+  strcpy(fifo_name, "CLIENT");
+
   return 0;
 }
